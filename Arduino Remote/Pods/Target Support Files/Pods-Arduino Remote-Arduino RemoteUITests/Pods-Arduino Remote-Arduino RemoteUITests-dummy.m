b72f2bf5f6d60d48d93ee79b588b0c7ff9473dca
Kurkin Oleksii <oleksii.kurkin@airslate.com>
2024-04-28 19:27:54 +0300

    - clean

--- Arduino Remote/Pods/Target Support Files/Pods-Arduino Remote-Arduino RemoteUITests/Pods-Arduino Remote-Arduino RemoteUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Arduino_Remote_Arduino_RemoteUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Arduino_Remote_Arduino_RemoteUITests
-@end
--- Arduino Remote/Pods/Target Support Files/Pods-Arduino RemoteTests/Pods-Arduino RemoteTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Arduino_RemoteTests : NSObject
-@end
-@implementation PodsDummy_Pods_Arduino_RemoteTests
-@end
