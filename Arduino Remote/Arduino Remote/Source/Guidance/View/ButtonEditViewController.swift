9f18bd1cf18b4695fe0a1ebf48ef5af949cdfc24
Kurkin Oleksii <oleksii.kurkin@airslate.com>
2024-05-30 23:42:04 +0300

    - menu completed

--- Arduino Remote/Arduino Remote/Source/Guidance/View/ButtonEditViewController.swift
+++ Arduino Remote/Arduino Remote/Source/Guidance/View/ButtonEditViewController.swift
@@ -6,16 +6,20 @@
 //
 
 import UIKit
+import RxSwift
+import RxRelay
 
 extension ButtonEditViewController {
     
-    class func instantiate() -> UIViewController {
+    class func instantiate(selectedMode: ButtonNodeMode, pin: Int) -> ButtonEditViewController {
         
         let storyboard = UIStoryboard(name: "Guidance", bundle: nil)
         let identifier = "ButtonEditViewController"
         
         let viewContoller = storyboard.instantiateViewController(withIdentifier: identifier)
                                 as! ButtonEditViewController
+        viewContoller.selectedMode.accept(selectedMode)
+        viewContoller.selectedPin.accept(pin)
         return viewContoller
     }
 }
@@ -23,6 +27,13 @@ extension ButtonEditViewController {
 class ButtonEditViewController: UIViewController {
     
     @IBOutlet weak var pinPickerView: UIPickerView!
+    @IBOutlet weak var segmentedViewController: UISegmentedControl!
+    
+    private let disposeBag = DisposeBag()
+    
+    let selectedMode = BehaviorRelay(value: ButtonNodeMode.oneClick)
+    let selectedPin = BehaviorRelay(value: 0)
+    let deleteEvent = PublishSubject<Void>()
     
     override func viewDidLoad() {
         
@@ -30,6 +41,18 @@ class ButtonEditViewController: UIViewController {
         
         pinPickerView.delegate = self
         pinPickerView.dataSource = self
+        pinPickerView.selectRow(selectedPin.value, inComponent: 0, animated: false)
+        
+        segmentedViewController.selectedSegmentIndex = selectedMode.value.intIndex
+        segmentedViewController.rx.selectedSegmentIndex
+            .subscribe(onNext: { [weak self] index in
+                self?.selectedMode.accept(.mode(for: index))
+        }).disposed(by: disposeBag)
+    }
+    
+    @IBAction func deleteButtonTapped(_ sender: Any) {
+        deleteEvent.onNext(())
+        deleteEvent.onCompleted()
     }
 }
 
@@ -53,5 +76,11 @@ extension ButtonEditViewController: UIPickerViewDelegate, UIPickerViewDataSource
               attributes: [
                 .font: UIFont(name: "HelveticaNeue-Bold", size: 26.0)!
               ])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView,
+                    didSelectRow row: Int,
+                    inComponent component: Int) {
+        selectedPin.accept(row)
     }
 }
